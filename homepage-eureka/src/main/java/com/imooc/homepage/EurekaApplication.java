5d3ed0f33be4cd46f5bbeeab65aaba0397f9c9df
taoxudong <taoxudong@oraro.net>
2019-10-09 11:59:12 +0800

    单节点 Eureka Server 的开发
    Change-Id: Ifa9c8be3b1735660ef26c3915f3b8c414b2b4ba5

---
+++ homepage-eureka/src/main/java/com/imooc/homepage/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.homepage;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 1. 只需要使用 @EnableEurekaServer 注解就可以让应用变为 Eureka Server
+ * 2. pom 文件中对应到 spring-cloud-starter-netflix-eureka-server
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
